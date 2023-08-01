3183da670deabcd8951923d9b76a500e247e8b84
alibei <sebastiankotzias83@gmail.com>
2023-08-01 09:17:24 +0200

    Handle FTP-related exceptions

--- ftp_push.py
+++ ftp_push.py
@@ -5,10 +5,9 @@ import tarfile
 import time
 
 # Set up FTP connection
-ftp = ftplib.FTP('ChangeMe-IP', 'ChangeMe-Username', 'ChangeMe-Password')
-
-# Change working dir to the FTP push dir
-ftp.cwd('/')
+def setup_ftp_connection():
+    ftp = ftplib.FTP('ChangeMe-IP', 'ChangeMe-Username', 'ChangeMe-Password')
+    return ftp
 
 # Set up the working dirs
 push_dir = '/home/ftp_push/ftp'
@@ -16,44 +15,58 @@ work_dir = '/home/ftp_push/work'
 extract_dir = os.path.join(work_dir, 'extracted')
 
 while True:
-    # Get a list of all tar.gz files in the push dir
-    archive_files = [f for f in os.listdir(push_dir) if f.endswith('.tar.gz')]
+    try:
+        # Set up FTP connection
+        ftp = setup_ftp_connection()
 
-    if archive_files:
-        # Sort the list of archive files by modification time (oldest first)
-        archive_files.sort(key=lambda f: os.path.getmtime(os.path.join(push_dir, f)))
+        # Change working dir to the FTP push dir
+        ftp.cwd('/')
 
-        # Extract each archive file one at a time
-        for archive_file in archive_files:
-            # Extract the archive to the extracted dir
-            with tarfile.open(os.path.join(push_dir, archive_file), 'r:gz') as tar:
-                tar.extractall(extract_dir)
+        # Get a list of all tar.gz files in the push dir
+        archive_files = [f for f in os.listdir(push_dir) if f.endswith('.tar.gz')]
 
-            # Remove the inverter files from the extracted dir
-            for filename in os.listdir(extract_dir):
-                if filename.startswith('inverter') and (filename.endswith('.nsf') or filename.endswith('.nmf')):
-                    os.remove(os.path.join(extract_dir, filename))
+        if archive_files:
+            # Sort the list of archive files by modification time (oldest first)
+            archive_files.sort(key=lambda f: os.path.getmtime(os.path.join(push_dir, f)))
 
-            # Create a new tar.gz archive with the same name as the extracted archive
-            new_archive_file = os.path.join(work_dir, archive_file)
-            with tarfile.open(new_archive_file, 'w:gz') as tar:
-                for root, dirs, files in os.walk(extract_dir):
-                    for file in files:
-                        tar.add(os.path.join(root, file), arcname=file)
+            # Extract each archive file one at a time
+            for archive_file in archive_files:
+                # Extract the archive to the extracted dir
+                with tarfile.open(os.path.join(push_dir, archive_file), 'r:gz') as tar:
+                    tar.extractall(extract_dir)
 
-            # Push the new archive file to the FTP server
-            with open(new_archive_file, 'rb') as f:
-                ftp.storbinary('STOR ' + archive_file, f)
+                # Remove the inverter files from the extracted dir
+                for filename in os.listdir(extract_dir):
+                    if filename.startswith('inverter') and (filename.endswith('.nsf') or filename.endswith('.nmf')):
+                        os.remove(os.path.join(extract_dir, filename))
 
-            # Delete the extracted files and the new archive file
-            os.remove(new_archive_file)
-            shutil.rmtree(extract_dir)
+                # Create a new tar.gz archive with the same name as the extracted archive
+                new_archive_file = os.path.join(work_dir, archive_file)
+                with tarfile.open(new_archive_file, 'w:gz') as tar:
+                    for root, dirs, files in os.walk(extract_dir):
+                        for file in files:
+                            tar.add(os.path.join(root, file), arcname=file)
 
-            # Remove the original archive file from the push dir
-            os.remove(os.path.join(push_dir, archive_file))
+                # Push the new archive file to the FTP server
+                with open(new_archive_file, 'rb') as f:
+                    ftp.storbinary('STOR ' + archive_file, f)
+
+                # Delete the extracted files and the new archive file
+                os.remove(new_archive_file)
+                shutil.rmtree(extract_dir)
+
+                # Remove the original archive file from the push dir
+                os.remove(os.path.join(push_dir, archive_file))
+
+        # Close the FTP connection
+        ftp.quit()
+
+    except ftplib.all_errors as e:
+        # Handle FTP-related exceptions
+        print(f"FTP Error: {e}")
+        print("Retrying FTP connection in 10 seconds...")
+        time.sleep(10)
+        continue
 
     # Wait 10 seconds
     time.sleep(10)
-
-# Close the FTP connection
-ftp.quit()
